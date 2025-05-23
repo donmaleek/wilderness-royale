febe6cc74a5d8368802d7f00a97d016da437ceca
donmaleek <bamboloshari@gmail.com>
2025-05-23 19:39:59 +0300

    updating package,json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,40 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+import type { NextConfig } from 'next'
+import { Configuration } from 'webpack'
+
+/** 
+ * @type {import('next').NextConfig}
+ */
+const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   assetPrefix: './',
   images: {
     unoptimized: true,
   },
-  experimental: {
-    optimizeCss: true
+  webpack: (
+    config: Configuration, 
+    { isServer }: { isServer: boolean }
+  ) => {
+    if (!isServer) {
+      config.resolve = config.resolve || {}
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        fs: false,
+        path: require.resolve('path-browserify'),
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify')
+      }
+    }
+    return config
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+ 
 }
 
 module.exports = nextConfig
